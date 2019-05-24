a51832efef8a92717bc86fe87da2f4c3d5160dbb
Xiao Li <xli87@bu.edu>
2019-05-24 18:32:08 -0400

    update

--- src/robot_cooking/run_automata_node.py
+++ src/robot_cooking/run_automata_node.py
@@ -7,6 +7,7 @@ import sys
 import time
 from future.utils import viewitems
 import copy
+import zmq
 
 from std_msgs.msg import String
 import ast
@@ -14,9 +15,20 @@ from robot_cooking_msgs.msg import DictArray
 
 default_config = {
     'init_node': True,
+    'msg_system': "ros",
+    'nb_robot': 1,
     'Automata':{
         'type': None,
         'config': None
+    },
+    'ZMQ': {
+        'pub': {
+            'aut_output': None
+        },
+        'sub': {
+            'baxter_skill_arg': None,
+            'jaco_skill_arg': None
+        }
     }
 }
 
@@ -37,7 +49,8 @@ class RunAutomataNode(object):
 
         #### initialize field var ####
         self.robot_skill_args = {}
-
+        self.zmq_robot_skill_args = {}
+        
         #### initialize automata ####
         self.aut = self.RunAutomataNode_config['Automata']['type'](self.RunAutomataNode_config['Automata']['config'])
         
@@ -52,8 +65,27 @@ class RunAutomataNode(object):
         self.skill_arg_subs = {}
         for robot in ['jaco', 'baxter']:
             self.skill_arg_subs[robot] = rospy.Subscriber(robot+"_skill_arg", DictArray, self.cb_skill_arg_subs, (robot))
-        # rospy.Subscriber("jaco_skill_arg", DictArray, self.cb_skill_arg_subs, queue_size=1)
-        
+
+        #### ZMQ ####
+        self.zmq_pub = None
+        self.zmq_sub = None
+        if self.RunAutomataNode_config['ZMQ'] is not None:
+            self.context = zmq.Context()
+            #### zmq pubs ####
+            self.zmq_pub = self.context.socket(zmq.PUB)
+            self.zmq_pub.bind("tcp://*:%s" %self.RunAutomataNode_config['ZMQ']['pub']['aut_output'])
+            
+            #### zmp subs ####
+            self.zmq_sub = self.context.socket(zmq.SUB)
+            self.zmq_sub.connect("tcp://burobotics:%s" %self.RunAutomataNode_config['ZMQ']['sub']['baxter_skill_arg'])
+            addr = "tcp://burobotics:{}".format(self.RunAutomataNode_config['ZMQ']['sub']['jaco_skill_arg'])
+            self.zmq_sub.connect(addr)
+            self.zmq_sub.setsockopt(zmq.SUBSCRIBE, "")
+
+
+    def construct_combined_skill_args(self):
+        combined_skill_args = {}
+            
     def cb_skill_arg_subs(self, msg, robot):
         converted_dict = {}
         for kv in msg.data:
@@ -64,46 +96,81 @@ class RunAutomataNode(object):
             converted_dict[key] = v
         self.robot_skill_args[robot] = converted_dict
 
-        skill_args = copy.copy(self.robot_skill_args[robot])
-        action_n_constraint, done = self.aut.step(skill_args)
-
-        if not done:
-            #### pub skills output by automata ####
-            ee_goal = action_n_constraint['make_hotdog']['node_action']['ee_goal']
-            gripper_action = action_n_constraint['make_hotdog']['node_action']['gripper_action']
-            other_action = action_n_constraint['make_hotdog']['node_action']['other_action']
-
-            automata_output = str(ee_goal) + "-" + str(gripper_action) + "-" + str(other_action) + "-" + str(done)            
-        else:
-            automata_output = "None-None-None-True"
         
-        # automata_output = 'moveto_condiment_condimentpre-opengripper-None-False'
-        self.skill_pub.publish(automata_output)
-
-        #### pub node and edges ####
-        curr_node_edge = self.aut.curr_node_edge
-        curr_node = curr_node_edge['make_hotdog']['node']
-        curr_edge = curr_node_edge['make_hotdog']['edge']
-
-        node_edge_msg = curr_node + "-" + str(curr_edge)
-     
-        self.aut_node_and_edge_pub.publish(node_edge_msg)
+        #### ZMQ ####
+        if self.zmq_sub is not None:
+            robot_name, robot_skill_args = self.zmq_sub.recv_json()
+            self.zmq_robot_skill_args[robot_name] = robot_skill_args
             
     def run(self):
         while not rospy.is_shutdown():
-            self.skill_pub.publish("None-None-None-True")
-            #rospy.spin()
+            if self.RunAutomataNode_config['msg_system'] == 'ros':
+                robot_skill_args = self.robot_skill_args
+            elif self.RunAutomataNode_config['msg_system'] == 'zmq':
+                robot_skill_args = self.zmq_robot_skill_args
+            else:
+                raise ValueError("unsupported msg_system")
+                
+            if len(self.robot_skill_args.keys()) > 0:
+                skill_args = copy.copy(self.robot_skill_args['jaco'])
+                action_n_constraint, done = self.aut.step(skill_args)
 
+                if not done:
+                    #### pub skills output by automata ####
+                    ee_goal = action_n_constraint['make_hotdog']['node_action']['ee_goal']
+                    gripper_action = action_n_constraint['make_hotdog']['node_action']['gripper_action']
+                    other_action = action_n_constraint['make_hotdog']['node_action']['other_action']
+                
+                    automata_output = str(ee_goal) + "-" + str(gripper_action) + "-" + str(other_action) + "-" + str(done)            
+                else:
+                    automata_output = "None-None-None-True"
+            else:
+                automata_output = "None-None-None-True"
+            # automata_output = 'moveto_condiment_condimentpre-opengripper-None-False'
+            self.skill_pub.publish(automata_output)
+
+            #### pub node and edges ####
+            curr_node_edge = self.aut.curr_node_edge
+            curr_node = curr_node_edge['make_hotdog']['node']
+            curr_edge = curr_node_edge['make_hotdog']['edge']
+            
+            node_edge_msg = curr_node + "-" + str(curr_edge)
+            
+            self.aut_node_and_edge_pub.publish(node_edge_msg)
+
+            #### ZMQ ####
+            if self.zmq_pub is not None:
+                self.zmq_pub.send_string("%s %s" %("automata_output", automata_output))
+
+            
 if __name__ == "__main__":
     from execution.execution_config import ExecutionConfig
 
+
+    aut_out_pub_port = str(sys.argv[1])
+    jaco_sub_port = str(sys.argv[2])
+    baxter_sub_port = str(sys.argv[3])
+    msg_system = str(sys.argv[4])
+    nb_robot = int(sys.argv[5])
+    
     exe_config = ExecutionConfig({'robot': None, 'init_node': False})
     automata_cls_type, automata_cls_config = exe_config.low_level_tl_skill_config()
 
     #### init class ####
     cls_config = default_config
+    cls_config['nb_robots'] = nb_robot
+    cls_config['msg_system'] = msg_system
     cls_config['Automata']['type'] = automata_cls_type
     cls_config['Automata']['config'] = automata_cls_config
+    cls_config['ZMQ'] = {
+        'pub': {
+            'aut_output': aut_out_pub_port
+        },
+        'sub': {
+            'baxter_skill_arg': baxter_sub_port,
+            'jaco_skill_arg': jaco_sub_port
+        }
+    }
 
     cls = RunAutomataNode(config=cls_config)
 
--- src/robot_cooking/run_robot_node.py
+++ src/robot_cooking/run_robot_node.py
@@ -7,6 +7,7 @@ import sys
 import time
 import ast
 from future.utils import viewitems
+import zmq
 
 from std_msgs.msg import String
 from robot_cooking_msgs.msg import DictArray
@@ -19,6 +20,14 @@ default_config = {
     'RobotCooking': {
         'type': None,
         'config': None
+    },
+    'ZMQ':{
+        'pub': {
+            'skill_args': None
+        },
+        'sub': {
+            'aut_output': None
+        }
     }
 }
 
@@ -38,10 +47,16 @@ class RunRobotNode(object):
 
         self.create_publishers_and_subscribers()
 
-        self.ee_goal = None
-        self.gripper_action = None
-        self.other_action = None
-        self.done = 'False'
+        self.ros_ee_goal = None
+        self.ros_gripper_action = None
+        self.ros_other_action = None
+        self.ros_done = 'False'
+
+        self.zmq_ee_goal = None
+        self.zmq_gripper_action = None
+        self.zmq_other_action = None
+        self.zmq_done = 'False'
+      
         
     def create_publishers_and_subscribers(self):
         #### publishers ####
@@ -50,25 +65,37 @@ class RunRobotNode(object):
         #### subscribers ####
         self.skill_to_execute_sub = rospy.Subscriber("automata_output", String, self.cb_skill_to_execute_sub, queue_size=1)
 
-        
+        self.zmq_pub = None
+        self.zmq_pub = None
+        if self.RunRobotNode_config['ZMQ'] is not None:
+            self.context = zmq.Context()
+            #### zmq pubs ####
+            self.zmq_pub = self.context.socket(zmq.PUB)
+            self.zmq_pub.bind("tcp://*:%s" %self.RunRobotNode_config['ZMQ']['pub']['skill_args'])
+            
+            #### zmp subs ####
+            self.zmq_sub = self.context.socket(zmq.SUB)
+            self.zmq_sub.connect("tcp://burobotics:%s" %self.RunRobotNode_config['ZMQ']['sub']['aut_output'])
+            self.zmq_sub.setsockopt(zmq.SUBSCRIBE, "")
+            
     def cb_skill_to_execute_sub(self, msg):
-        self.ee_goal, self.gripper_action, self.other_action, self.done = msg.data.split("-")
+        self.ros_ee_goal, self.ros_gripper_action, self.ros_other_action, self.ros_done = msg.data.split("-")
         # print("ee_goal:{}".format(ee_goal))
         # print("gripper_action:{}".format(gripper_action))
         # print("other_action:{}".format(other_action))
         # print("----------")
 
-        if self.ee_goal == "None":
-            self.ee_goal = None
-        if self.gripper_action == "None":
-            self.gripper_action = None
-        if self.other_action == "None":
-            self.other_action = None
+        if self.ros_ee_goal == "None":
+            self.ros_ee_goal = None
+        if self.ros_gripper_action == "None":
+            self.ros_gripper_action = None
+        if self.ros_other_action == "None":
+            self.ros_other_action = None
 
-         
     def update_and_publish_skill_arg(self):
         self.robot_cooking.update_skill_arg()
         skill_arg_dict_array = DictArray()
+        skill_arg_dict_processed = {} # transform np.ndarray to list
         for k, v in viewitems(self.robot_cooking.skill_arg):
             kv = KeyValue()
             kv.key = k
@@ -86,6 +113,11 @@ class RunRobotNode(object):
 
             kv.value = str(v_processed)
             skill_arg_dict_array.data.append(kv)
+
+            skill_arg_dict_processed[k] = v_processed
+            
+        if self.zmq_pub is not None:
+            self.zmq_pub.send_json((self.robot, skill_arg_dict_processed))
             
         self.skill_arg_pub.publish(skill_arg_dict_array)
             
@@ -93,20 +125,50 @@ class RunRobotNode(object):
         while not rospy.is_shutdown():
             for _ in range(200):
                 self.update_and_publish_skill_arg()
+                ## aut skill output through zmq
+                if self.zmq_sub is not None:
+                    topic, data = self.zmq_sub.recv_string().split()
+                    if topic == 'automata_output':
+                        aut_output = data
+                        self.zmq_ee_goal, self.zmq_gripper_action, self.zmq_other_action, self.zmq_done = aut_output.split('-')
+                        if self.zmq_ee_goal == "None":
+                            self.zmq_ee_goal = None
+                        if self.zmq_gripper_action == "None":
+                            self.zmq_gripper_action = None
+                        if self.zmq_other_action == "None":
+                            self.zmq_other_action = None
 
-            if self.done != 'True':    
-                self.robot_cooking.execute_automata_output(ee_goal=self.ee_goal,
-                                                           gripper_action=self.gripper_action,
-                                                           other_action=self.other_action,
-                                                           dry_run=self.RunRobotNode_config['dry_run'])
+
+                if self.RunRobotNode_config['msg_system'] == 'ros':
+                    ee_goal = self.ros_ee_goal
+                    gripper_action = self.ros_gripper_action
+                    other_action = self.ros_other_action
+                    done = self.ros_done
+                elif self.RunRobotNode_config['msg_system'] == 'zmq':
+                    ee_goal = self.zmq_ee_goal
+                    gripper_action = self.zmq_gripper_action
+                    other_action = self.zmq_other_action
+                    done = self.zmq_done
+                else:
+                    raise ValueError('msg system unsupported')
+                    
+                    
+            # if done != 'True':    
+            #     self.robot_cooking.execute_automata_output(ee_goal=ee_goal,
+            #                                                gripper_action=gripper_action,
+            #                                                other_action=other_action,
+            #                                                dry_run=self.RunRobotNode_config['dry_run'])
        
 if __name__ == "__main__":
     from execution.execution_config import ExecutionConfig
     from execution.run_robot_cooking import RunRobotCooking
     
     robot_name = str(sys.argv[1])
-
     dry_run = str(sys.argv[2])
+    zmq_pub_port = str(sys.argv[3])
+    aut_sub_port = str(sys.argv[4])
+    msg_system = str(sys.argv[5])
+    
     if dry_run == 'true' or dry_run == 'True':
         dry_run = True
     elif dry_run == 'false' or dry_run == 'False':
@@ -123,7 +185,17 @@ if __name__ == "__main__":
     cls_config['dry_run'] = dry_run
     cls_config['RobotCooking']['type'] = RunRobotCooking
     cls_config['RobotCooking']['config'] = run_robot_cooking_config
-
+    cls_config['msg_system'] = msg_system
+    
+    cls_config['ZMQ'] = {
+        'pub': {
+            'skill_args': zmq_pub_port
+        },
+        'sub': {
+            'aut_output': aut_sub_port
+        }
+    }
+    
     cls = RunRobotNode(config=cls_config)
 
     #### run ####
